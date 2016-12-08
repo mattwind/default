21f9058c65fa91897745482f23e731fa09efc6fa
mwind <mattwind@gmail.com>
2016-12-08 14:10:09 -0800

    check for root

--- post-setup.sh
+++ post-setup.sh
@@ -2,7 +2,12 @@
 
 echo "Starting Post Setup"
 
-su -c 'apt-get update'
-su -c 'apt-get install visudo git vim'
-su -c 'usermod -a -G sudo mwind'
-
+if [[ $EUID -ne 0 ]];
+then
+  echo "This script must be run as root" 
+  exit 1
+else
+  apt-get update
+  apt-get install visudo git vim
+  usermod -a -G sudo mwind
+fi
