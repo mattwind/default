2fb8ba6c688a3abaee3e04576410ae6f298fa73d
mwind <mattwind@gmail.com>
2016-12-08 14:16:34 -0800

    functions

--- post-setup.sh
+++ post-setup.sh
@@ -7,8 +7,22 @@ then
   echo "This script must be run as root" 
   exit 1
 else
+  upgrade  
+  clean
+  quit
+fi
+
+function quit {
+  exit
+}
+
+function clean {
+  apt-get clean
+}
+
+function upgrade {
   apt-get update
   apt-get upgrade -y
   apt-get install visudo git vim -y
   usermod -a -G sudo mwind
-fi
+}
